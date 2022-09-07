9fa5498a1e70701f36ded90da700572a40f32682
shinjuno123 <shinjuno123@naver.com>
2022-09-07 21:59:16 +0900

    Level 1 - Username and Password only

---
+++ app.js
@@ -0,0 +1,77 @@
+"use strict";
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const port = 3000;
+
+const app = express();
+
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = {
+    email : String,
+    password : String
+}
+
+const User = mongoose.model("User", userSchema);
+
+
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register",async function(req, res){
+    const newUser = new User({
+        email:req.body.username,
+        password : req.body.password
+    });
+
+    try{
+        await newUser.save();
+        res.render("secrets");
+    } catch (err){
+        console.log(err);
+    }
+    
+});
+
+app.post("/login", async function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try{
+        const foundUser = await User.findOne({email:username});
+        if (foundUser){
+            if (foundUser.password === password){
+                res.render("secrets");
+            }
+        }
+    } catch (err){
+        console.log(err);
+    }
+
+});
+
+
+app.listen(port, function(){
+    console.log(`Server is running on port ${port}`);
+});
